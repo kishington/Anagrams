62ae57fbc3e029f749680afc17f58c3277e0da06
Mihail Iarov <yarovmk@gmail.com>
2019-10-23 11:51:48 +0100

    Added src/test/WordManipulatorTest.java file

---
+++ src/test/java/ua/com/foxminded/anagrams/manipulators/WordManipulatorTest.java
@@ -0,0 +1,15 @@
+package ua.com.foxminded.anagrams.manipulators;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WordManipulatorTest {
+
+    @Test
+    void test() {
+        //fail("Not yet implemented");
+        System.out.println("This test ran");
+    }
+
+}
