f3c7a41d7dfc07cf6206f4bd05ba0d86fa957615
Mihail Iarov <yarovmk@gmail.com>
2019-10-23 15:13:19 +0100

    Added test case when input is null

--- src/test/java/ua/com/foxminded/anagrams/manipulators/WordManipulatorTest.java
+++ src/test/java/ua/com/foxminded/anagrams/manipulators/WordManipulatorTest.java
@@ -2,29 +2,32 @@ package ua.com.foxminded.anagrams.manipulators;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class WordManipulatorTest {
+    private static WordManipulator manipulator;
+
+    @BeforeAll
+    public static void initialise() {
+        manipulator = new WordManipulator();
+    }
 
     @Test
     void createAnagramOutputWhenPassedEmptyString() {
-        WordManipulator manipulator = new WordManipulator();
         String expected = "";
         String actual = manipulator.createAnagram("");
         assertEquals(expected, actual);
     }
-    
+
     @Test
     void createAnagramOutputWhenPassedJustWhitespaces() {
-        WordManipulator manipulator = new WordManipulator();
         String expected = "  \t   \n   \r   ";
         String actual = manipulator.createAnagram("  \t   \n   \r   ");
         assertEquals(expected, actual);
     }
-    
+
     @Test
     void createAnagramOutputWhenPassedJustOneWord() {
-        WordManipulator manipulator = new WordManipulator();
         String expected = "  ..aw=y$-kj  ";
         String actual = manipulator.createAnagram("  ..jk=y$-wa  ");
         assertEquals(expected, actual);
@@ -32,10 +35,15 @@ class WordManipulatorTest {
 
     @Test
     void createAnagramReturnSameStringWhenCalledTwoTimes() {
-        WordManipulator manipulator = new WordManipulator();
         String expected = " ..aw=y$-kj  35v#jtykj%45asf     0(afa_a(l  ";
         String actual = manipulator.createAnagram(" ..aw=y$-kj  35v#jtykj%45asf     0(afa_a(l  ");
         actual = manipulator.createAnagram(actual);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void createAnagramShouldThrowNullPointerExceptionWhenPassedNull() {
+        assertThrows(NullPointerException.class, () -> manipulator.createAnagram(null));
+    }
+
 }
