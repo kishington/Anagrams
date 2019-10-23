31faf88c5d7e11e5a826ba3d4ea7da6a5722132c
Mihail Iarov <yarovmk@gmail.com>
2019-10-23 13:26:35 +0100

    Added some tests createAnagram method

--- src/test/java/ua/com/foxminded/anagrams/manipulators/WordManipulatorTest.java
+++ src/test/java/ua/com/foxminded/anagrams/manipulators/WordManipulatorTest.java
@@ -7,9 +7,35 @@ import org.junit.jupiter.api.Test;
 class WordManipulatorTest {
 
     @Test
-    void test() {
-        //fail("Not yet implemented");
-        System.out.println("This test ran");
+    void createAnagramOutputWhenPassedEmptyString() {
+        WordManipulator manipulator = new WordManipulator();
+        String expected = "";
+        String actual = manipulator.createAnagram("");
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    void createAnagramOutputWhenPassedJustWhitespaces() {
+        WordManipulator manipulator = new WordManipulator();
+        String expected = "  \t   \n   \r   ";
+        String actual = manipulator.createAnagram("  \t   \n   \r   ");
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    void createAnagramOutputWhenPassedJustOneWord() {
+        WordManipulator manipulator = new WordManipulator();
+        String expected = "  ..aw=y$-kj  ";
+        String actual = manipulator.createAnagram("  ..jk=y$-wa  ");
+        assertEquals(expected, actual);
     }
 
+    @Test
+    void createAnagramReturnSameStringWhenCalledTwoTimes() {
+        WordManipulator manipulator = new WordManipulator();
+        String expected = " ..aw=y$-kj  35v#jtykj%45asf     0(afa_a(l  ";
+        String actual = manipulator.createAnagram(" ..aw=y$-kj  35v#jtykj%45asf     0(afa_a(l  ");
+        actual = manipulator.createAnagram(actual);
+        assertEquals(expected, actual);
+    }
 }
